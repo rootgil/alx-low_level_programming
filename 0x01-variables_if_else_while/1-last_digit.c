222f05fdde6467359ac7e8ae81d00912c1d1f962
rootgil <gillesahouantchede@gmail.com>
2022-03-14 01:41:32 +0100

    space after bracket 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,22 +2,20 @@
 #include <time.h>
 #include <stdio.h>
 /*
- main - compare number
+ *main - compare number
+ *return the type
 */
-/* betty style doc for function main goes there */
 int main(void)
 {
-	int n,l;
-	
+	int n, l;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
-	
 	if (l > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	else if (l == 0)
 		printf("Last digit of %d is %d and is 0\n", n, l);
-	else 
+	else
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	return (0);
 }
