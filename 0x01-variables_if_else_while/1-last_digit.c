678b316cc1ea67f3b782d6a5135c6996d74c3952
rootgil <gillesahouantchede@gmail.com>
2022-03-14 01:39:09 +0100

    space after bracket 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+/*
+ main - compare number
+*/
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -12,10 +14,10 @@ int main(void)
 	l = n % 10;
 	
 	if (l > 5)
-		printf("Last digit of %d is %d and is greater than 5\n",n,l);
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	else if (l == 0)
-		printf("Last digit of %d is %d and is 0\n",n,l);
+		printf("Last digit of %d is %d and is 0\n", n, l);
 	else 
-		printf("Last digit of %d is %d ans is less than 6 and not 0\n",n,l);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	return (0);
 }
