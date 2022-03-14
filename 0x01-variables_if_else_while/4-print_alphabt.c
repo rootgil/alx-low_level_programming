7ed2f957e3e4e07aeae614ba962fda0f54978d7d
rootgil <gillesahouantchede@gmail.com>
2022-03-14 01:53:06 +0100

    space after bracket 0

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,23 @@
+
 #include <stdio.h>
-/*
-	MAIN and RETURN
-*/
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Prints the alphabet without q and e.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char alp[26]="abcdfghijklmnoprstuvwxyz";
-	for(int i=0;i<24;i++)
+	int i;
+
+	for (i = 97; i < 123; i++)
 	{
-		putchar(alp[i]);
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
 	}
 	putchar('\n');
 	return (0);
