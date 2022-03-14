52d02cd4c5b391a1cbe633002bc7f09cd8f87592
rootgil <gillesahouantchede@gmail.com>
2022-03-14 01:43:31 +0100

    space after bracket 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- *main - compare number
- *return the type
-*/
+
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n, l;
